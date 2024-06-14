38ee11fa4a5dbd33941914acd245f17295351c47
sooraj12 <sooraj.cs@cogknit.com>
2024-06-14 20:05:01 +0530

    cosine learning rate decay

--- gpt2/model.py
+++ gpt2/model.py
@@ -190,8 +190,28 @@ model.to(device)
 model = torch.compile(model)
 
 
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+
+def get_lr(it):
+
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+
+    if it > max_steps:
+        return min_lr
+
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+
+    return min_lr + coeff * (max_lr - min_lr)
+
+
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8)
-for i in range(50):
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
@@ -202,13 +222,17 @@ for i in range(50):
 
     loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
     dt = t1 - t0
     tokens_per_sec = train_loader.B * train_loader.T
     print(
-        f"step {i}, loss: {loss.item():.6f}, norm: {norm:.4f},  dt: {dt*1000:.2f}ms, tok/sec: {tokens_per_sec/dt:.2f}"
+        f"step {step}, loss: {loss.item():.6f}, norm: {norm:.4f},  dt: {dt*1000:.2f}ms, tok/sec: {tokens_per_sec/dt:.2f}"
     )
 
 
