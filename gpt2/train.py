a7397785bf9a5b2c36b9128f42e55c836b49e37b
sooraj12 <sooraj.cs@cogknit.com>
2024-06-17 23:11:27 +0530

    changed checkpoint name

--- gpt2/train.py
+++ gpt2/train.py
@@ -75,7 +75,7 @@ def train():
     # raw_model = model.module if ddp else model
 
     # load pretrained model weights(checkpoints)
-    model_weights_path = os.path.join("log", "gpt2_v1_10000.pt")
+    model_weights_path = os.path.join("log", "gpt2_v1.pt")
     checkpoint = torch.load(model_weights_path, map_location="cpu")
     csd = checkpoint["model"]
     model = GPT(GPTConfig(vocab_size=50304)).to(device)
